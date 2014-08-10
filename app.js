2dc49e9afe6d172c15614b1ca46b3329bc3aa210
Jonathan King <jonathan@jonathan-VirtualBox.(none)>
2014-08-10 14:58:00 -0500

    Writing socket objects

--- app.js
+++ app.js
@@ -16,29 +16,116 @@ app.get('/js/core.js', function (req, res) {
 
 });
 
+var gameIDIndex = 0;
 var arrSockets = [];
+var newgame;
+var newsock;
 
 io.sockets.on('connection', function (socket) {
 
-	socket.emit('news', "Welcome to Baseball Strategy II");
+	newsock = new UserSocket();
+	newsock.sock = socket;
+	newsock.game = null;
+	newsock.host = null;
+	newsock.opponent = null;
+	newsock.room = "";
 
-	socket.on('clientData', function (data) {
-
-		ParseClientData(data);
-
-	});
+	newsock.Output('news', 'Welcome to Baseball Strategy II');
 
 	arrSockets.push(socket);
 
 });
 
-function ParseClientData(data) {
+CompleteGame = function ()
 
-	var arrData = data.split("||");
+function Game() {
 
-	for (i = 0; i < arrData.length; i++) {
+	this.gameID = ++gameIDIndex;
+	this.complete = false;
+
+}
+
+function UserSocket() {
+
+	this.sock.on('clientData', function (data)) {
+
+		this.ParseClientData;
+
+	};
+
+	this.sock.on('broadcast', function (data)) {
+
+		this.BroadcastToGame;
+
+	};
+
+
+	this.ParseClientData = function (data) {
+
+		var arrData = data.split("--");
+		var command = arrData[0];
+		var data = arrData[1];
+		var dataValues = data.split(",");
+
+		var aGameData = {};
+
+		for (i = 0; i < dataValues.length; i++) {
+
+			var kvpair = dataValues[i].split("|");
+			aGameData.kvpair[0] = kvpair[1];
+
+		}
+
+		this.PerformAction(command, aGameData);
+
+	}
+
+
+	this.PerformAction = function (command, aGameData) {
+
+		switch (command) {
+
+			case "newgame":
+				this.CreateGame(aGameData);
+				break;
+			case "joingame":
+				this.JoinGame(aGameData);
+				break;
+
+		}
+
+	}
+
+	this.CreateGame = function (startdata) {
+
+		this.game = new Game();
+		this.game.host = this.sock;
+
+	} 
+
+	this.JoinGame = function (joindata) {
+
+		gameid = joindata.id;
 
 		
+
+
+	}
+
+	this.BroadcastToGame = function(data) {
+
+		if (this.game > 0) {
+
+			this.sock.broadcast.emit(data);
+
+		}
+
+	}
+
+	this.Output = function(data) {
+
+		this.sock.broadcast.emit(data);
+
 	}
 }
 
